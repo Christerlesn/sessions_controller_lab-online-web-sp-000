22cd1e0c71e25873dd925d699dbbea6f2c7c2289
Christerle Nounoute <christerlesn@gmail.com>
2021-05-13 18:47:00 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:name] == nil
+    if params[:name] == nil || params[:name].empty?
       redirect_to "/login"
     elsif session[:name] = params[:name]
       redirect_to "/"
